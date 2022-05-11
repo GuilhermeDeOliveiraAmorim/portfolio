e02f4150ae2a581c91ee35883fb78c4d50e7f005
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-05-11 06:20:24 -0300

    feat: dir settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './pages/home';
+import Projects from './pages/projects';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
+    <Projects />
   </React.StrictMode>
 );
