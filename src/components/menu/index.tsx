35f2b4f52f6877c214bc318f00979aea9e2e51eb
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2022-05-11 12:44:45 -0300

    feat: create menu component structure

---
+++ src/components/menu/index.tsx
@@ -0,0 +1,17 @@
+export default function Menu() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    Projects
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+        </nav>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Footer from './components/footer';
+import Menu from './components/menu';
 import './index.css';
+import Contact from './pages/contact';
 import Home from './pages/home';
 import Projects from './pages/projects';
 
@@ -9,7 +12,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <Menu />
     <Home />
     <Projects />
+    <Contact />
+    <Footer />
   </React.StrictMode>
 );
