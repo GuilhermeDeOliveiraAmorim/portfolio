776fd76071c424acb44cbca98052696a3414e0c8
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-05-12 23:47:47 -0300

    feat: home adjustments

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,19 @@
+import Credentials from "components/perfil/credentials";
+import Photo from "components/perfil/photo";
 import StyleHome from "./Home.module.scss";
 
 export default function Home() {
     return (
         <div className={StyleHome.home}>
             <div className={StyleHome.perfil}>
-                PERFIL
+                <Photo />
+                <Credentials />
             </div>
             <div className={StyleHome.menu}>
-                MENU
+                
             </div>
             <div className={StyleHome.content}>
-                CONTENT
+                
             </div>
         </div>
     )
