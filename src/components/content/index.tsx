00306d75dbddd0e87b99a66325ae9199a00f8c1b
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-05-14 18:36:53 -0300

    feat: create content component

---
+++ src/components/content/index.tsx
@@ -0,0 +1,9 @@
+import StyleContent from './Content.module.scss';
+
+export default function Content() {
+    return (
+        <div className={StyleContent.content}>
+            Content
+        </div>
+    )
+}
